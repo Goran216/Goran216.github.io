0652424bfb359a71421a52984b62ed48343c28be
Goran Sherko <goransjawdat@gmail.com>
2017-02-22 16:33:42 +0300

    adding more cool stuff

--- comeat-website/js/script.js
+++ comeat-website/js/script.js
@@ -1,3 +1,5 @@
+// script for popup login div
+
 jQuery(document).ready(function($){
 	//open popup
 	$('.cd-popup-trigger').on('click', function(event){
@@ -19,3 +21,72 @@ jQuery(document).ready(function($){
 	    }
     });
 });
+
+// end
+
+
+
+// script for opening profile dorpdown from navigation bar
+$('.drop-profile-items').hide();
+$("#showButton").click(function(e){
+     $('.drop-profile-items').toggle(0);//or just show instead of toggle
+});
+// end
+
+
+
+
+// SCRIPT FOR TABS IN SEARCH RESULT PAGE
+	 $(".tab_content").hide();
+	 $(".tab_content:first").show();
+
+ /* if in tab mode */
+	 $("ul.tabs li").click(function() {
+
+		 $(".tab_content").hide();
+		 var activeTab = $(this).attr("rel");
+		 $("#"+activeTab).fadeIn();
+
+		 $("ul.tabs li").removeClass("active");
+		 $(this).addClass("active");
+
+	 $(".tab_drawer_heading").removeClass("d_active");
+	 $(".tab_drawer_heading[rel^='"+activeTab+"']").addClass("d_active");
+
+	 });
+ /* if in drawer mode */
+ $(".tab_drawer_heading").click(function() {
+
+		 $(".tab_content").hide();
+		 var d_activeTab = $(this).attr("rel");
+		 $("#"+d_activeTab).fadeIn();
+
+	 $(".tab_drawer_heading").removeClass("d_active");
+		 $(this).addClass("d_active");
+
+	 $("ul.tabs li").removeClass("active");
+	 $("ul.tabs li[rel^='"+d_activeTab+"']").addClass("active");
+	 });
+
+
+ /* Extra class "tab_last"
+		to add border to right side
+		of last tab */
+ $('ul.tabs li').last().addClass("tab_last");
+
+ // END OF SCRIPT FOR TABS
+
+
+
+
+// FUNCTION FOR ACTIVING HEART COLOR AT SEARCH RESULT PAGE
+
+ $('#heart-icon').click(function changeclass() {
+		if ($('#heart-icon').hasClass('active-heart')){
+            $('#heart-icon').removeClass('active-heart');
+    } else {
+		     $(this).addClass('active-heart');
+		}
+});
+
+// END OF FUNCTION
